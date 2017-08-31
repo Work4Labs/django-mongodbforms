9ecbca039ee22e28c097cdd88fbf131a6686634e
Adrien Siebert <asiebert@work4labs.com>
2017-08-30 19:41:50 -0700

    BAT-7558 Field generator: much like help_text, verbose_name is optional

--- mongodbforms/fieldgenerator.py
+++ mongodbforms/fieldgenerator.py
@@ -115,7 +115,7 @@ class MongoFormFieldGenerator(object):
         return value.lower() == 'true'
 
     def get_field_label(self, field):
-        if field.verbose_name:
+        if hasattr(field, 'verbose_name') and field.verbose_name:
             return capfirst(field.verbose_name)
         if field.name is not None:
             return capfirst(create_verbose_name(field.name))
